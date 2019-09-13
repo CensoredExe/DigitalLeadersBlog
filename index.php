5ac45fb9accfb61d214935adf20e981f1c4d26b2
Oliver Crowe <olivercampbellcrowe@outlook.com>
2019-09-13 15:14:59 +0100

    Initial files, front end

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <!-- Base meta tags, dont touch. -->
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+        <!-- Custom meta tags, change at will. -->
+        <title>Digital Leader Blog & News | South Bromsgrove High School</title>
+        <meta name="description" content="ENTER DESCRIPTION HERE">
+        <meta name="keywords" content="ENTER KEYWORDS HERE">
+        <meta name="author" content="Oliver Crowe">
+
+        <!-- Links, CSS, Fonts, Icons and frameworks -->
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700&display=swap" rel="stylesheet"> 
+        <link rel="stylesheet" href="css/style.css">
+
+    </head>
+    <body>
+    <header>
+    <nav>
+        <div class="row">
+            <div class="logo-hold">
+                <p class="logo"><a href="">Digital Leaders</a></p>
+            </div>
+
+            <ul class="items-hold">
+                <li class="list-li"><a href="#" class="list-a">SBHS</a></li>
+                <li class="list-li"><a href="#" class="list-a">FAQ</a></li>
+                <li class="list-li"><a href="admin/signup/" class="list-a">Login</a></li>
+            </ul>
+        </div>
+    </nav>
+        <div class="txt-box">
+            <h1 class="title">Digital Leaders</h1>
+            <p class="sub-title">Blog and News</p>
+        </div>
+    </header>
+
+    <!-- Javascript + JQuery -->
+    <script
+    src="https://code.jquery.com/jquery-3.4.1.min.js"
+	integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+	crossorigin="anonymous"></script>
+    <script src="js/index.js"></script>
+    </body>
+    
+</html>
